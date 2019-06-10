581a96fd09d2c02447088e1fab7787455847f57c
akgvn <akgvn@yandex.com>
2019-06-10 10:18:12 +0300

    More experiments

--- src/jsontest.py
+++ src/jsontest.py
@@ -3,8 +3,10 @@ import json
 f_test = open('test.json', 'r')
 f_write = open('nt.json', 'w')
 
-data = f_test.read() # Reads the data as string, I guess?
+data = f_test.read() # Reads the data as a string, I guess?
 
-j_data = json.loads(data) # Deserializes the data into a Dictionary-like structure.
+j_data = json.loads(data) # Deserializes the data into a Dictionary structure.
 
-print(j_data["glossary"]["GlossDiv"]["GlossList"]["GlossEntry"])
+for d in iter(j_data.values()):
+    print(d)
+    f_write.write(json.dumps(d, indent=4)) # Writes to file, indent=4 prettifies the json file.
