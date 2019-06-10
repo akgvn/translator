61ae96f2958d1727277ed003c908ec5c395b782c
akgvn <akgvn@yandex.com>
2019-06-10 15:28:16 +0300

    Can strip tags

--- src/find.py
+++ src/find.py
@@ -1,24 +1,35 @@
 # Finds the key - value pairs in a JSON file to translate.
 # Usually key is "tr"
 
+import html
+
 import json
+
+from tag_stripper import strip_tags
 
 jsonFileName: str = 'test.json'
 targetLocale = "en"
 
-json_file = open(jsonFileName)
+json_file = open(jsonFileName, encoding='utf8')
 
 file_data = json_file.read()
 file_data = json.loads(file_data)
 
-output = open("out.json", "w")
+output = open("out.json", "w", encoding='utf8')
 
 json_file.close()
 
 def translate(stri):
+    # TODO don't try to translate the empty strings!
+    
+    stri = strip_tags(stri)
+
+    stri = html.unescape(stri)
     return "Translated: " + stri # TODO the real thing
 
 def traverseAndPrint(file_data):
+    # Recursive implementation to find strings in a JSON file for translating.
+
     if isinstance(file_data, list):
         for elem in file_data:
             traverseAndPrint(elem)
@@ -28,19 +39,17 @@ def traverseAndPrint(file_data):
 
             if isinstance(data, list):
                 traverseAndPrint(data)
+            elif (isinstance(data, dict)) and ("tr" in data.keys()):
+                data[targetLocale] = translate(data["tr"])  # Do the translation
             elif key == "title":
-                # TODO stuff
                 if isinstance(data, str):
                     title_dict = {"tr" : data}
-                else:
+                elif isinstance(data, dict):
                     title_dict = data
                 
-                title_dict[targetLocale] = translate(title_dict["tr"])
-                
-                file_data[key] = title_dict
-            elif key == "tr":
-                file_data[targetLocale] = translate(file_data["tr"])
+                title_dict[targetLocale] = translate(title_dict["tr"]) # Do the translation
+                file_data[key] = title_dict # Append translation to the dictionary
 
 traverseAndPrint(file_data)
 
-output.write(json.dumps(file_data, indent=4, ensure_ascii=False))
+output.write(json.dumps(file_data, indent=4, ensure_ascii=False)) # Dump the dictionary into a JSON file
---
+++ src/tag_stripper.py
@@ -0,0 +1,18 @@
+# Source: https://stackoverflow.com/questions/11061058/using-htmlparser-in-python-3-2
+
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
